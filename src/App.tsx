6107615915f0b35bf9816221cdfd596d5a5d2bbe
BukreevSergey <bukreevwork2@gmail.com>
2023-11-16 18:08:23 +0200

    add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Header} from "./layot/header/Header";
+import {Maine} from "./layot/section/maine/Maine";
+import {TechStack} from "./layot/section/techStack/TechStack";
+import {AboutMe} from "./layot/section/aboutme/AboutMe";
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Maine />
+        <AboutMe />
+      <TechStack />
 
     </div>
   );
---
+++ src/styles/Theme.tsx
@@ -0,0 +1,14 @@
+export const Theme = {
+    colors: {
+        primaryBg: "#1F1F20",
+        secondaryBg: "#252527",
+        accent:" #7572D5",
+        font: "#FFF",
+        borderColor: "#4A4A4A",
+        placeholderColor: "495057",
+        fontSecond: "linear-gradient(to left, #360dde, #0dbad7)",
+        fontThird: "#A7A7A7"
+    }
+
+}
+
