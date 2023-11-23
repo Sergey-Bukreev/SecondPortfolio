f0f5e6d121549497a9f2ab26bbf00ccc2cc9cc89
BukreevSergey <bukreevwork2@gmail.com>
2023-11-23 15:25:05 +0200

    refactoring EDUCATION block

--- src/layot/section/education/Education.tsx
+++ src/layot/section/education/Education.tsx
@@ -4,16 +4,22 @@ import {SectionTitle} from "../../../components/SectionTitle";
 import {Card} from "./card/Card";
 import {S} from "./Education_Styles"
 
+const EducationData = [
+    {speciality: "Automated Control Systems & Electric Drive", place:"Kharkov Polytechnic Institute", period:"Aug 2012 - Dec 2018", location:"Kharkov" },
+
+    {speciality:"Front And Developer", place:"IT_INCUBATOR", period:"Nov 2023 - today", location:"Minsk"}
+]
+
 
 export const Education = () => {
+
+    const listEducation = EducationData.map((ed: any) => <Card key={"index"} {...ed}/>)
+
     return (
         <S.Education>
             <Container>
                 <SectionTitle>Education</SectionTitle>
-                <Card speciality={"Automated Control Systems & Electric Drive"}  place={"Kharkov Polytechnic Institute"} period={"Aug 2012 - Dec 2018"} location={"Kharkov"} />
-                <Card speciality={"Front And Developer"} place={"IT_INCUBATOR"} period={"Nov 2023 - today"} location={"Minsk"} />
-
-
+                {listEducation}
             </Container>
 
         </S.Education>
