473fe4006378daa73f6fa26c4c11737a8befacc6
BukreevSergey <bukreevwork2@gmail.com>
2023-11-23 15:25:35 +0200

    refactoring CARD for EDUCATION block

--- src/layot/section/education/Education_Styles.ts
+++ src/layot/section/education/Education_Styles.ts
@@ -10,31 +10,36 @@ const Card = styled.div`
   grid-template-columns: 1fr;
   grid-template-rows: auto;
   gap: 8px;
-  padding: 16px;
+  padding: 26px;
   align-items: center;
   
   width: 80%;
   margin: 0 auto;
- 
+  
 
-  position: relative;
-  &::before {
-    content: "";
-    display: inline-block;
-    width: 100%;
-    height: 2px;
-    background-color: ${Theme.colors.accent};
-    position: absolute;
-    bottom: -1px;
+  border-bottom: 2px solid ${Theme.colors.accent};
+  @media ${Theme.media.mobile} {
     
-    @media ${Theme.media.mobile} {
-    bottom: -18px;
-    }
   }
+
+  // position: relative;
+  // &::before {
+  //   content: "";
+  //   display: inline-block;
+  //   width: 100%;
+  //   height: 2px;
+  //   background-color: ${Theme.colors.accent};
+  //   position: absolute;
+  //   bottom: -1px;
+  //  
+  //   @media ${Theme.media.tablet} {
+  //   bottom: -24px;
+  //   }
+  // }
     
   
 `
-const Speciality = styled.p`
+const Speciality = styled.span`
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -44,19 +49,14 @@ const Speciality = styled.p`
   line-height: 28px;
 `
 
-const Place = styled.p`
-  font-size: 12px;
-  margin-right: 20px;
-`
-const Location = styled.p``
-const Date = styled.span`
+
+
+const Text = styled.span`
   font-size: 12px;
 `
 export const S ={
     Education,
     Card,
     Speciality,
-    Place,
-    Location,
-    Date
+    Text
 }
--- src/layot/section/education/card/Card.tsx
+++ src/layot/section/education/card/Card.tsx
@@ -1,35 +1,35 @@
 import React from 'react';
-import {FlexWrapper} from "../../../../components/FlexWrapper";
-import {Icon} from "../../../../components/icon/Icon";
-import {S} from "../Education_Styles"
-type CardPropsType ={
-    speciality: string
-   location: string
-    place: string
-    period: string
-}
+import { FlexWrapper } from "../../../../components/FlexWrapper";
+import { Icon } from "../../../../components/icon/Icon";
+import { S } from "../Education_Styles";
 
- export const Card = (props:CardPropsType) => {
+type CardPropsType = {
+    speciality: string;
+    place: string;
+    location: string;
+    period: string;
+};
+
+ export const Card = (data: CardPropsType) => {
+    const iconData = [
+        { iconId: "building", value: data.place },
+        { iconId: "location", value: data.location },
+        { iconId: "calendar", value: data.period },
+    ];
+
     return (
         <S.Card>
-
-                <S.Speciality>{props.speciality}</S.Speciality>
+            <S.Speciality>{data.speciality}</S.Speciality>
             <FlexWrapper align={"center"} justify={"space-between"} wrap={"wrap"}>
-                <FlexWrapper align={"center"} justify={"space-between"} wrap={"wrap"} >
-                    <Icon iconId={"building"} width={"16"} height={"12"} viewBox={"0, 0, 16, 12"} />
-                    <S.Place>{props.place}</S.Place>
-
-                    <Icon iconId={"location"} width={"16"} height={"12"} viewBox={"0, 0, 16, 12"} />
-                    <S.Location>{props.location}</S.Location>
-                </FlexWrapper>
-
-                <FlexWrapper align={"center"} justify={"space-between"} wrap={"wrap"}>
-                    <Icon iconId={"calendar"} width={"16"} height={"12"} viewBox={"0, 0, 16, 12"} />
-                     <S.Date>{props.period}</S.Date>
-                </FlexWrapper>
-
+                {iconData.map((item) => (
+                    <FlexWrapper key={item.iconId} align={"center"} justify={"space-around"} wrap={"wrap"}>
+                        <Icon iconId={item.iconId} width={"16"} height={"12"} viewBox={"0, 0, 16, 12"} />
+                        <S.Text>{item.value}</S.Text>
+                    </FlexWrapper>
+                ))}
             </FlexWrapper>
         </S.Card>
     );
 };
+
 
