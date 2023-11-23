37b98f7e934d1d5fc7083942eb1313a9624316e6
BukreevSergey <bukreevwork2@gmail.com>
2023-11-23 18:54:10 +0200

    refactor Social List

--- src/layot/header/socialList/SocialList.tsx
+++ src/layot/header/socialList/SocialList.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import {Icon} from "../../../components/icon/Icon";
-import {S} from "./SocialList_Styles"
+import { Icon } from "../../../components/icon/Icon";
+import { S } from "./SocialList_Styles";
 
-export const SocialList:React.FC = () => {
+type SocialIcon = {
+    id: string;
+    iconId: string;
+};
+
+const socialIcons: SocialIcon[] = [
+    { id: "github", iconId: "github" },
+    { id: "twitter", iconId: "twitter" },
+    { id: "linkedin", iconId: "linkd" },
+];
+
+export const SocialList: React.FC = () => {
     return (
         <S.SocialList>
-            <S.SocialItem>
-                <S.SocialLink>
-                    <Icon iconId={"github"} width={"32"} height={"32"} viewBox={"0, 0, 32, 32"}  />
-                </S.SocialLink>
-            </S.SocialItem>
-            <S.SocialItem>
-                <S.SocialLink>
-                    <Icon iconId={"twitter"} width={"32"} height={"32"} viewBox={"0, 0, 32, 32"} />
-                </S.SocialLink>
-            </S.SocialItem>
-            <S.SocialItem>
-                <S.SocialLink>
-                    <Icon iconId={"linkd"} width={"32"} height={"32"} viewBox={"0, 0, 32, 32"} />
-                </S.SocialLink>
-            </S.SocialItem>
+            {socialIcons.map((social) => (
+                <S.SocialItem key={social.id}>
+                    <S.SocialLink>
+                        <Icon iconId={social.iconId} width={"32"} height={"32"} viewBox={"0, 0, 32, 32"} />
+                    </S.SocialLink>
+                </S.SocialItem>
+            ))}
         </S.SocialList>
     );
 };
-
