a50a1e0b1fff41e281b6708ab99f5ba3b8b09393
BukreevSergey <bukreevwork2@gmail.com>
2023-11-24 11:44:07 +0200

    change footer

--- src/layot/footer/Footer.tsx
+++ src/layot/footer/Footer.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import styled from "styled-components";
-import {DesktopMenu} from "../header/headerMenu/desktopMenu/DesktopMenu";
 import {Theme} from "../../styles/Theme";
 import {Container} from "../../components/Container";
-import {FlexWrapper} from "../../components/FlexWrapper";
+
 const items = ["Home", "About", "Tech Stack", "Project", "Contact"]
 export const Footer:React.FC = () => {
     return (
         <StyledFooter>
             <Container>
 
-                <Copyright>© 2023 Sergey Bukreev, All Rights Reserved.</Copyright>
+                <Copyright>© Designed and built by <span>Sergey Bukreev</span> with <span>Love</span> & <span>Coffee</span></Copyright>
 
             </Container>
 
@@ -25,10 +24,14 @@ const Copyright = styled.small`
   font-family: Poppins, serif;
   font-size: 18px;
   
+  
   font-weight: 400;
   line-height: 26px;
   
-  background:${Theme.colors.fontSecond};
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
+  color: white;
+  span {background:${Theme.colors.fontSecond};
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;}
+  
+  
 `
